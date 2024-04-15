026949156cf7c9dd8070ec50107a4868cb089846
Serhii Kuznets <testopesto7@gmail.com>
2024-04-15 20:41:46 +0200

    chnges for performance site

--- src/js/menu.js
+++ src/js/menu.js
@@ -49,21 +49,21 @@
     const section = document.querySelector(sectionId);
     if (section) {
       section.scrollIntoView({ behavior: 'smooth' });
-      toggleMenu(); // Закрыть меню после прокрутки
+      toggleMenu(); 
     }
   };
 
   closeMenuLink.forEach(item => {
     item.addEventListener('click', (event) => {
-      event.preventDefault(); // Предотвратить стандартное поведение ссылки
-      const targetSectionId = item.getAttribute('href'); // Получить целевой идентификатор секции
-      scrollToSection(targetSectionId); // Прокрутить к секции
+      event.preventDefault(); 
+      const targetSectionId = item.getAttribute('href'); 
+      scrollToSection(targetSectionId); 
     });
   });
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
-  // Close the mobile menu on wider screens if the device orientation changes
+  
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
