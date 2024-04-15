538bde90ad398aa6b0cf8605d8e2126c99b8ee92
Serhii Kuznets <testopesto7@gmail.com>
2024-04-15 18:01:22 +0200

    changes for performances

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,37 @@
+// (() => {
+//   const mobileMenu = document.querySelector('.js-menu-container');
+//   const openMenuBtn = document.querySelector('.js-open-menu');
+//   const closeMenuBtn = document.querySelector('.js-close-menu');
+//     const closeMenuLink = document.querySelectorAll('.header-link');
+
+//   const toggleMenu = () => {
+//     const isMenuOpen =
+//       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+//     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+//     mobileMenu.classList.toggle('is-open');
+
+//     const scrollLockMethod = !isMenuOpen
+//       ? 'disableBodyScroll'
+//       : 'enableBodyScroll';
+//     bodyScrollLock[scrollLockMethod](document.body);
+//   };
+//   closeMenuLink.forEach(item => item.addEventListener('click', toggleMenu));
+//   openMenuBtn.addEventListener('click', toggleMenu);
+//   closeMenuBtn.addEventListener('click', toggleMenu);
+
+//   // Close the mobile menu on wider screens if the device orientation changes
+//   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+//     if (!e.matches) return;
+//     mobileMenu.classList.remove('is-open');
+//     openMenuBtn.setAttribute('aria-expanded', false);
+//     bodyScrollLock.enableBodyScroll(document.body);
+//   });
+// })();
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-    const closeMenuLink = document.querySelectorAll('.header-link');
+  const closeMenuLink = document.querySelectorAll('.header-link');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -15,7 +44,22 @@
       : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
   };
-  closeMenuLink.forEach(item => item.addEventListener('click', toggleMenu));
+
+  const scrollToSection = (sectionId) => {
+    const section = document.querySelector(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+      toggleMenu(); // Закрыть меню после прокрутки
+    }
+  };
+
+  closeMenuLink.forEach(item => {
+    item.addEventListener('click', (event) => {
+      event.preventDefault(); // Предотвратить стандартное поведение ссылки
+      const targetSectionId = item.getAttribute('href'); // Получить целевой идентификатор секции
+      scrollToSection(targetSectionId); // Прокрутить к секции
+    });
+  });
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
@@ -27,3 +71,5 @@
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
+
+
